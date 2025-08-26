605ef38b41eaa0c0035aaf5312a18b82b0d214e3
Asgar Ali <asgarali11104@gmail.com>
2025-08-26 12:54:36 +0900

    Upload Latihan 2

---
+++ KULIAH/DART/project_easymeal/bin/BasicTest.dart
@@ -0,0 +1,22 @@
+void main (List<String> args) {
+/*
+  Set<int> nums = {1, 2, 3, 4, 5};
+
+  print(nums.length);
+  print(nums.add(6));
+  print(nums.remove(1));
+  print(nums.contains(1));
+  print(nums);
+*/
+  List Buah = ['apel', 'jeruk', 'mangga', 'pisang'];
+
+  print(Buah);
+  print(Buah[0]);
+  print(Buah[2]);
+  print(Buah[3]);
+
+  print(Buah.length);
+  Buah.addAll(['semangka', 'pepaya']);
+  print(Buah.indexOf('apel'));
+  Buah.forEach(())
+}
